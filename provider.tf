78e95eb758a75b2e57c30e05984b37b57423ac49
riskRover <mr.sp3799@gmail.com>
2024-12-08 16:57:49 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,14 +4,9 @@ terraform {
       source  = "kreuzwerker/docker"
       version = "3.0.2"
     }
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.11.0"
-    }
   }
 }
 
 provider "docker" {
   host = "unix:///var/run/docker.sock"
 }
-provider "aws" {}
